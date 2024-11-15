e1bd36131827690794d1041ad3080c65cb71c040
aDJi2003 <adjidharmawan87@gmail.com>
2024-11-15 09:37:43 +0700

    test: tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-
 const App = () => {
   return (
-    <div>
-      <p className='text-3xl text-red-400'>Hello World</p>
-    </div>
+    <>
+      <p className="text-3xl text-red-500">Hello World</p>
+    </>
   );
 }
 
