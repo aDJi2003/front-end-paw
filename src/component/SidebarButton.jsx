03df3e7af72d83eb9ac7680dfcf3792f5617e66b
aDJi2003 <adjidharmawan87@gmail.com>
2024-11-24 08:24:39 +0700

    feat: rename navbar to sidebar and add modal for logout

--- src/component/SidebarButton.jsx
+++ src/component/SidebarButton.jsx
@@ -1,15 +1,23 @@
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const NavBarButton = ({ to, icon: Icon, label }) => {
+const SidebarButton = ({ to, icon: Icon, label, onClick }) => {
+  const handleClick = (e) => {
+    if (onClick) {
+      e.preventDefault();
+      onClick();
+    }
+  };
+
   return (
     <NavLink
       to={to}
+      onClick={handleClick}
       className={({ isActive }) =>
         `flex items-center px-4 py-2 rounded transition ${
           isActive
-            ? "bg-purple-600 text-white"
-            : "hover:bg-purple-200 hover:text-gray-900"
+            ? "bg-[#FF2DF7] text-white"
+            : "hover:bg-[#fb77f6] hover:text-gray-900"
         }`
       }
     >
@@ -18,10 +26,11 @@ const NavBarButton = ({ to, icon: Icon, label }) => {
   );
 };
 
-NavBarButton.propTypes = {
+SidebarButton.propTypes = {
   to: PropTypes.string.isRequired,
   icon: PropTypes.object.isRequired,
   label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
 
-export default NavBarButton;
+export default SidebarButton;
